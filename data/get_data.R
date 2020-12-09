9c6f2cd197d165e7af932f912a03c018f3ceab1d
emchristensen <erica.christensen@weecology.org>
2020-12-09 09:33:03 -0700

    reran get data

--- data/get_data.R
+++ data/get_data.R
@@ -11,7 +11,7 @@
 #'     changed SPCO4 -> SPFL2
 #'     changed SPCR -> SPFL2
 #'     changed ARPA9, ARPU9, ARPUL -> ARIST
-#' last run: 12/4/20
+#' last run: 12/10/20
 
 library(dplyr)
 library(lubridate)
